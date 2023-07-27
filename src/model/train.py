09fd9bbe19557d729e6eea1a3434f0a75c027f23
Pedro Cobas <pedro.cobas@musala.com>
2023-07-27 17:43:20 +0300

    Configure Github Actions with Azure Machine Learning.

--- src/model/train.py
+++ src/model/train.py
@@ -7,6 +7,7 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
@@ -22,6 +23,11 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def get_csvs_df(path):
